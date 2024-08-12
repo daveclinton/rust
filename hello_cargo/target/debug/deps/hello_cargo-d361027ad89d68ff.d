50f96248afc22aa5fd6a170b44eda29f6e976a5c
David Clinton <clinton@192.168.0.103>
2024-08-12 13:22:10 +0300

    feat: learn cargo conventions

---
+++ hello_cargo/target/debug/deps/hello_cargo-d361027ad89d68ff.d
@@ -0,0 +1,5 @@
+/Users/clinton/Desktop/learn-rust/hello_cargo/target/debug/deps/hello_cargo-d361027ad89d68ff: src/main.rs
+
+/Users/clinton/Desktop/learn-rust/hello_cargo/target/debug/deps/hello_cargo-d361027ad89d68ff.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/clinton/Desktop/learn-rust/hello_cargo/target/debug/hello_cargo: /Users/clinton/Desktop/learn-rust/hello_cargo/src/main.rs
